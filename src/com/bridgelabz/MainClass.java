59a0d4fdfa01a257f2396bd2624548be978972b4
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-03-29 12:54:14 +0530

    Update MainClass.java

--- src/com/bridgelabz/MainClass.java
+++ src/com/bridgelabz/MainClass.java
@@ -3,10 +3,10 @@ package com.bridgelabz;
 public class MainClass {
 
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.push(56);
-        list.push(30);
-        list.push(70);
+         LinkedList list = new LinkedList();
+        list.add(56);
+        list.add(70);
+        list.insertAfter(56, 30);
         list.display();
     }
 }
